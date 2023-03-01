117fb838d618dec9b1ccade7568f504c42f15b0b
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2023-03-01 22:08:16 +0900

    exec i3 without dbus

--- gentooSetup.sh
+++ gentooSetup.sh
@@ -2,7 +2,7 @@
 #equery list "*" | grep dbus
 
 vi /etc/portage/make.conf
-#USE="X dbus"
+#USE="X"
 echo 'VIDEO_CARDS="intel"' >> /etc/portage/make.conf
 emerge -q xorg-drivers
 
@@ -12,12 +12,10 @@ emerge -qDU @world
 
 emerge -q dmenu i3 i3status i3lock xfce4-terminal
 
-echo -e 'if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
-\texec startx
-fi' >> .bash_profile
+echo 'exec startx' >> .bash_profile
 echo 'exec i3' > ~/.xinitrc
 
-rc-update add dbus default
-/etc/init.d/dbus start
+#rc-update add dbus default
+#/etc/init.d/dbus start
 
 emerge -q neofetch sudo vim
