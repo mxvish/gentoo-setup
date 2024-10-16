ac761552c50f9ea096ffa0f7dbaa1683ca1e8ffa
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2024-10-16 19:09:09 +0900

    shorten code

--- gentooSetup.sh
+++ gentooSetup.sh
@@ -10,19 +10,36 @@ mkdir /mnt/arch
 mv eduroam.8021x /var/lib/iwd
 #edit edoroam.8021x
 
+euse -E alsa
+
+eselect repository add brave-overlay git https://gitlab.com/jason.oliveira/brave-overlay.git
+emerge --sync -q brave-overlay
+echo "dev-libs/libpthread-stubs **" >> /etc/portage/package.accept_keywords/libpthread-stubs
+echo 'dev-python/setuptools python_targets_python3_11' > /etc/portage/package.use/setuptools
+echo ">=app-i18n/fcitx-qt-5.1.5-r2 qt6" > /etc/portage/package.use/fcitx
+echo "app-i18n/mozc fcitx5" > /etc/portage/package.use/mozc
+
+#echo "net-im/zoom all-rights-reserved" >> /etc/portage/package.license
+#echo "net-im/zoom ~amd64" >> /etc/portage/package.accept_keywords/net-im-zoom
+
 getuto
+emerge -DUuq @world
 
 packages=(
+    alsa-utils
+    brave-bin::brave-overlay
     dev-python/pip
     dev-vcs/git
     dmenu
     eselect-repository
     exfatprogs
+    fcitx-config-tool
     gentoolkit
     i3
     i3lock
     i3status
     links
+    mozc
     neofetch
     noto-cjk
     noto-emoji
@@ -38,21 +55,6 @@ packages=(
 )
 
 for i in "${packages[@]}"; do emerge -q "$i"; done
-
-euse -E alsa
-
-eselect repository add brave-overlay git https://gitlab.com/jason.oliveira/brave-overlay.git
-emerge --sync -q brave-overlay
-echo "dev-libs/libpthread-stubs **" >> /etc/portage/package.accept_keywords/libpthread-stubs
-echo 'dev-python/setuptools python_targets_python3_11' > /etc/portage/package.use/setuptools
-echo ">=app-i18n/fcitx-qt-5.1.5-r2 qt6" > /etc/portage/package.use/fcitx
-echo "app-i18n/mozc fcitx5" > /etc/portage/package.use/mozc
-
-#echo "net-im/zoom all-rights-reserved" >> /etc/portage/package.license
-#echo "net-im/zoom ~amd64" >> /etc/portage/package.accept_keywords/net-im-zoom
-
-emerge -DUuq @world
-emerge -q alsa-utils brave-bin::brave-overlay fcitx-config-tool mozc
 
 echo -e 'if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
 \texec startx
