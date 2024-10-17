cf6443bcff2b4eff536cab115b2ebdd412de7f88
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2024-10-17 09:54:30 +0900

    change owner of i3 config

--- gentooSetup.sh
+++ gentooSetup.sh
@@ -74,5 +74,6 @@ mv bashrc /home/$USER/.bashrc
 chown $USER:$USER /home/$USER/.bashrc
 
 mv config /home/$USER/.config/i3
+chown $USER:$USER /home/$USER/.config/i3/config
 wget -q https://raw.githubusercontent.com/mxvish/i3status/main/i3status.conf
 mv i3status.conf /etc/
