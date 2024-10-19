0e103174576e47343c9f8e36cb0dbf225d4ffbf7
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2024-10-19 19:05:22 +0900

    use abbreviation options

--- gentooInstall.sh
+++ gentooInstall.sh
@@ -21,12 +21,12 @@ tar xpf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
 echo 'MAKEOPTS="-j11 -l12"' >> etc/portage/make.conf
 
 cp -L /etc/resolv.conf /mnt/gentoo/etc/
-mount --types proc /proc /mnt/gentoo/proc
-mount --rbind /sys /mnt/gentoo/sys
+mount -t proc /proc /mnt/gentoo/proc
+mount -R /sys /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/sys
-mount --rbind /dev /mnt/gentoo/dev
+mount -R /dev /mnt/gentoo/dev
 mount --make-rslave /mnt/gentoo/dev
-mount --bind /run /mnt/gentoo/run
+mount -B /run /mnt/gentoo/run
 mount --make-slave /mnt/gentoo/run
 
 chroot /mnt/gentoo /bin/bash
